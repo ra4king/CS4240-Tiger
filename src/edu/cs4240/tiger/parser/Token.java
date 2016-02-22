5759f5e709429a2bf03c5682304eb35fed7b6118
Roi Atalla <roi@roiatalla.com>
2016-02-22 05:24:47 -0500

    Added token variable to Token class.

--- src/edu/cs4240/tiger/parser/Token.java
+++ src/edu/cs4240/tiger/parser/Token.java
@@ -5,11 +5,14 @@ package edu.cs4240.tiger.parser;
  */
 public class Token {
 	private TokenClass tokenClass;
+	private String token;
+	
 	private String line;
 	private int lineNum;
 	
-	public Token(TokenClass tokenClass, String line, int lineNum) {
+	public Token(TokenClass tokenClass, String token, String line, int lineNum) {
 		this.tokenClass = tokenClass;
+		this.token = token;
 		this.line = line;
 		this.lineNum = lineNum;
 	}
