6eee0ceb6116aee42c699a89c0b28c8ed88fc607
Roi Atalla <roi@roiatalla.com>
2016-03-07 04:18:23 -0500

    Moved functions around.

--- src/edu/cs4240/tiger/analyzer/TigerAnalyzer.java
+++ src/edu/cs4240/tiger/analyzer/TigerAnalyzer.java
@@ -33,6 +33,54 @@ public class TigerAnalyzer {
 		analyzeProgramStatements();
 	}
 	
+	private void analyzeFunctions() throws TigerParseException {
+		analyzeFunction((RuleNode)((RuleNode)ast.getChildren().get(1)).getChildren().get(2));
+	}
+	
+	private void analyzeFunction(RuleNode funcdecls) throws TigerParseException {
+		if(funcdecls.getChildren().size() == 0) {
+			return;
+		}
+		
+		RuleNode funcdecl = (RuleNode)funcdecls.getChildren().get(0);
+		Pair<RuleNode, List<Pair<String, RuleNode>>> funcInfo = symbolTable.getFunctions().get(((LeafNode)funcdecl.getChildren().get(1)).getToken().getToken());
+		if(!analyzeFunctionStmts(funcInfo, (RuleNode)funcdecl.getChildren().get(7)) && funcInfo.getKey() != null) {
+			throw new TigerParseException("Not all code paths return", ((LeafNode)funcdecl.getChildren().get(8)).getToken());
+		}
+		analyzeFunction((RuleNode)funcdecls.getChildren().get(1));
+	}
+	
+	private boolean analyzeFunctionStmts(Pair<RuleNode, List<Pair<String, RuleNode>>> funcInfo, RuleNode stmts) throws TigerParseException {
+		if(funcInfo == null) {
+			throw new IllegalStateException("funcInfo is null somehow...");
+		}
+		
+		HashMap<String, RuleNode> funcVarTypes = new HashMap<>();
+		funcVarTypes.putAll(symbolTable.getVariables());
+		funcInfo.getValue().forEach((Pair<String, RuleNode> p) -> funcVarTypes.put(p.getKey(), p.getValue()));
+		
+		RuleNode stmt = (RuleNode)((RuleNode)stmts.getChildren().get(0)).getChildren().get(0);
+		Node first = stmt.getChildren().get(0);
+		
+		boolean doesReturn = false;
+		
+		if(first instanceof LeafNode && ((LeafNode)first).getToken().getTokenClass() == TigerTokenClass.RETURN) {
+			RuleNode returnType = getNumexprType((RuleNode)stmt.getChildren().get(1), funcVarTypes);
+			if(funcInfo.getKey() == null || !isTypeCompatibleAssign((funcInfo.getKey()), returnType)) {
+				throw new TigerParseException("Type of returned expression does not match return type", ((LeafNode)first).getToken());
+			}
+			doesReturn = true;
+		} else {
+			analyzeStatement(stmt, funcVarTypes, funcInfo.getKey(), false);
+		}
+		
+		if(stmts.getChildren().size() > 1) {
+			doesReturn |= analyzeFunctionStmts(funcInfo, (RuleNode)stmts.getChildren().get(1));
+		}
+		
+		return doesReturn;
+	}
+	
 	private void analyzeProgramStatements() throws TigerParseException {
 		analyzeStatements((RuleNode)ast.getChildren().get(3), symbolTable.getVariables(), null, false);
 	}
@@ -184,53 +232,5 @@ public class TigerAnalyzer {
 					throw new TigerParseException("Unexpected statement", firstLeaf.getToken());
 			}
 		}
-	}
-	
-	private void analyzeFunctions() throws TigerParseException {
-		analyzeFunction((RuleNode)((RuleNode)ast.getChildren().get(1)).getChildren().get(2));
-	}
-	
-	private void analyzeFunction(RuleNode funcdecls) throws TigerParseException {
-		if(funcdecls.getChildren().size() == 0) {
-			return;
-		}
-		
-		RuleNode funcdecl = (RuleNode)funcdecls.getChildren().get(0);
-		Pair<RuleNode, List<Pair<String, RuleNode>>> funcInfo = symbolTable.getFunctions().get(((LeafNode)funcdecl.getChildren().get(1)).getToken().getToken());
-		if(!analyzeFunctionStmts(funcInfo, (RuleNode)funcdecl.getChildren().get(7)) && funcInfo.getKey() != null) {
-			throw new TigerParseException("Not all code paths return", ((LeafNode)funcdecl.getChildren().get(8)).getToken());
-		}
-		analyzeFunction((RuleNode)funcdecls.getChildren().get(1));
-	}
-	
-	private boolean analyzeFunctionStmts(Pair<RuleNode, List<Pair<String, RuleNode>>> funcInfo, RuleNode stmts) throws TigerParseException {
-		if(funcInfo == null) {
-			throw new IllegalStateException("funcInfo is null somehow...");
-		}
-		
-		HashMap<String, RuleNode> funcVarTypes = new HashMap<>();
-		funcVarTypes.putAll(symbolTable.getVariables());
-		funcInfo.getValue().forEach((Pair<String, RuleNode> p) -> funcVarTypes.put(p.getKey(), p.getValue()));
-		
-		RuleNode stmt = (RuleNode)((RuleNode)stmts.getChildren().get(0)).getChildren().get(0);
-		Node first = stmt.getChildren().get(0);
-		
-		boolean doesReturn = false;
-		
-		if(first instanceof LeafNode && ((LeafNode)first).getToken().getTokenClass() == TigerTokenClass.RETURN) {
-			RuleNode returnType = getNumexprType((RuleNode)stmt.getChildren().get(1), funcVarTypes);
-			if(funcInfo.getKey() == null || !isTypeCompatibleAssign((funcInfo.getKey()), returnType)) {
-				throw new TigerParseException("Type of returned expression does not match return type", ((LeafNode)first).getToken());
-			}
-			doesReturn = true;
-		} else {
-			analyzeStatement(stmt, funcVarTypes, funcInfo.getKey(), false);
-		}
-		
-		if(stmts.getChildren().size() > 1) {
-			doesReturn |= analyzeFunctionStmts(funcInfo, (RuleNode)stmts.getChildren().get(1));
-		}
-		
-		return doesReturn;
 	}
 }
