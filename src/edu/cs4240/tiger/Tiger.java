9d25f51d205fca654283615533eb606090ced793
Roi Atalla <roi@roiatalla.com>
2016-03-21 20:27:52 -0400

    Bumped Tiger Compiler version number to 1.0.2.

--- src/edu/cs4240/tiger/Tiger.java
+++ src/edu/cs4240/tiger/Tiger.java
@@ -14,7 +14,7 @@ import edu.cs4240.tiger.parser.TigerToken;
  * @author Roi Atalla
  */
 public class Tiger {
-	private static final String VERSION = "1.0.1";
+	private static final String VERSION = "1.0.2";
 	
 	private static void printUsage() {
 		System.out.println("Tiger Compiler v" + VERSION + " by Roi Atalla.\n");
